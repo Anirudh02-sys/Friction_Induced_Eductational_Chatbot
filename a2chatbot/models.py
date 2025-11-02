c657125723503daf34fa57700d7a5220cebe35dc
Anirudh Krishnan <anirudh.krishnan02@gmail.com>
2025-11-02 14:24:01 -0500

    Modifying Per user Assistant to Per Level Assitant

--- a2chatbot/models.py
+++ a2chatbot/models.py
@@ -16,10 +16,19 @@ class Participant(models.Model):
 
 
 class Assistant(models.Model):
-    assistant_id = models.TextField(verbose_name = "Assistant ID")
-    video_name = models.CharField(verbose_name= "videoname", default = '', max_length=100)
-    vector_store_id = models.TextField(verbose_name='Vector store ID')
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, primary_key=True)
+    LEVEL_CHOICES = [
+        ('beginner', 'Beginner'),
+        ('intermediate', 'Intermediate'),
+        ('advanced', 'Advanced'),
+    ]
+    level = models.CharField(max_length=30,default="beginner",choices=LEVEL_CHOICES)   # beginner / intermediate / advanced
+    assistant_id = models.TextField(verbose_name="Assistant ID")
+    video_name = models.CharField(verbose_name="videoname", default='', max_length=100)
+    vector_store_id = models.TextField(verbose_name='Vector store ID', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.level} - {self.video_name}"
+
 
 class ChatLog(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
